90b815d6e58d7679e19b4dfc5a2976fb9f737c7b
waka <koukikiki0116@gmail.com>
2020-02-23 14:15:18 +0900

    init

---
+++ swift_test/swift_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_test
+//
+//  Created by waka on 2020/02/23.
+//  Copyright © 2020 waka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
