5bdf20c924f4df3c4fc0c1afcf9b385ad3d9f9a6
waka <koukikiki0116@gmail.com>
2020-02-23 13:23:56 +0900

    delete

--- swift_test/swift_test/ViewController.swift
+++ swift_test/swift_test/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    
+    @IBAction func changeLabel(sender: AnyObject) {
+        testLabel.text = "Hello World"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
