e1e7ac851f109f75eefda5bd7e358a4d5847c792
sato-hiroyuki-7 <satouhiroyuki721721@gmail.com>
2020-02-23 13:33:26 +0900

    swift

---
+++ testApp_20200223/testApp_20200223/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  testApp_20200223
+//
+//  Created by 佐藤周之 on 2020/02/23.
+//  Copyright © 2020 hiroyuki sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var label: UILabel!
+    
+}
+
